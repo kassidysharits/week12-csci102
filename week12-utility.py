81751146f07024975acf08eb02ecd1dd9ba06fe9
kassidysharits <kassidysharits@mymail.mines.edu>
2019-11-21 16:53:37 -0700

    FindWordCount function

--- week12-utility.py
+++ week12-utility.py
@@ -20,3 +20,15 @@ def UpdateString(string1, string2, index):
             string3 += string1[x]
     return string3
 
+def FindWordCount(lists,word):
+    string1 = ""
+    count = 0
+    for x in range(len(lists)):
+        string1 += lists[x]
+        string1 += ' '
+    lists = string1.split()
+    for x in range(len(lists)):
+        if lists[x] == word:
+            count += 1
+    return count
+
