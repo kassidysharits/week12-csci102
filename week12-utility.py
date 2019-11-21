182f82c1fb717e80cb1c6b204030d8cdc26f463c
kassidysharits <kassidysharits@mymail.mines.edu>
2019-11-21 16:50:23 -0700

    LoadFile function

--- week12-utility.py
+++ week12-utility.py
@@ -1,2 +1,13 @@
+# Kassidy Sharits
+# CSCI 102 - Section B
+# Week 12
+
 def PrintOutput(output):
     print('OUTPUT ' + output)
+
+def LoadFile(filename):
+    file = open(filename, 'r+')
+    contents = file.read()
+    lines = contents.splitlines()
+    return lines
+
